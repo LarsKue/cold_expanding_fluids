b7a6bf31f4aa826153f5c3a3bda2cdb28b6b2b63
Lars Kue <lars@kuehmichel.de>
2020-12-09 15:35:35 +0100

    Updated Tests

--- kinetic_theory/tests.py
+++ kinetic_theory/tests.py
@@ -3,6 +3,7 @@ import unittest
 
 
 class TestVec3(unittest.TestCase):
+    # TODO
     pass
 
 
@@ -14,17 +15,40 @@ class TestParticles(unittest.TestCase):
         import particles
         self.instance = particles.Particles()
 
-    def test_add_particle(self):
-        from particles import Vec3, Particles
-        self.instance.add_particle(Vec3(0.0, 0.0, 0.0), Vec3(0.0, 0.0, 0.0), 1.0)
+        # need at least one particle to perform other tests
+        self.instance.add_particle(particles.Vec3(0.0, 0.0, 0.0), particles.Vec3(0.0, 0.0, 0.0), 1.0)
 
     def test_run(self):
         from particles import Vec3
 
-        for i in range(1000):
+        for i in range(10):
             self.instance.add_particle(Vec3(i, i, 0.0), Vec3(0.0, 0.0, 0.0), 1.0)
 
-        self.instance.run(n=1000, h=0.01)
+        self.instance.run(n=100, h=0.01)
+
+    def test_potential(self):
+        def potential(v):
+            return v
+
+        self.instance.set_potential(potential)
+
+
+
+        self.instance.run(n=100, h=0.01)
+
+        self.instance.unset_potential()
+
+    def test_error(self):
+        def potential_err(_v):
+            raise ValueError("I can't compute that")
+
+        self.instance.set_potential(potential_err)
+
+        with self.assertRaises(ValueError):
+            self.instance.run(n=100, h=0.01)
+
+        self.instance.unset_potential()
+
 
 
 if __name__ == "__main__":
